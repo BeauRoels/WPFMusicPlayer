9d9bf345d0cfdbae601fd43acf68c19f1965d34f
Beau Roels <beauroels@hotmail.be>
2021-11-29 10:23:53 +0100

    Uploading musicplayer project

---
+++ ProjectMusicPlayer-BeauRoels/Project/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace ConsoleMusicPlayerBR
+{
+    class Program
+
+    {
+        //controls
+        public const string PAUSE = "pauze";
+        public const string ADD = "toevoegen";
+        public const string STOP = "stop";
+        public const string MUTE = "mute";
+        public const string UNMUTE = "unmute";
+        public const string PLAY = "play";
+        public const string NEXT = "next";
+        public const string PREVIOUS = "previous";
+        public const string BEWERK = "bewerk";
+        public const string VERWIJDER = "verwijder";
+        public const string VOEGTOE = "voegtoe";
+        public const string TERUG = "terug";
+        public const string VOLUME = "volume";
+        public const string CONTINUE = "continue";
+        public const string REQUIREMENT = ".mp3";
+        //Windows media player
+        public static WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public static string docsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string destination = System.IO.Path.Combine(docsFolder, "playlist.txt ");
+        public static string musicLocation = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+        //inputs
+        public static string input;
+        public static string song;
+        public static int sizeOfList;
+        //lists and arrays
+        public static string[] linesArray;
+        public static List<string> songList = new List<string>();
+        public static List<string> songPathList = new List<string>();
+        static void Main(string[] args)
+        {
+
+            // player.URL = System.IO.Path.Combine(getLocation(), song);
+           
+            while(input != STOP)
+            {
+                geefStartmenu();
+                switch (input)
+                {
+                    case PLAY:
+                        Console.WriteLine("geef het nummer dat je wil spelen 'song.mp3'");
+                        ReadSong();
+                        Play(song, musicLocation);
+                        break;
+                    case PAUSE:
+                        Console.WriteLine("je liedje is gepauzeerd");
+                        Pause();
+                        break;
+
+                    case BEWERK:
+                        PlaylistMenu();
+                        break;
+                    case MUTE:
+                        Mute(true);
+                        break;
+                    case NEXT:
+                        player.URL = System.IO.Path.Combine(musicLocation, GetPrevious());
+                        Console.WriteLine($"huidig liedje {song.Substring(musicLocation.Length + 1)}");
+                        break;
+                    case PREVIOUS:
+                        player.URL = System.IO.Path.Combine(musicLocation, GetNextSong());
+                        Console.WriteLine($"huidig liedje {song.Substring(musicLocation.Length + 1)}");
+                        break;
+                        
+                    case UNMUTE:
+                        Mute(false);
+                        break;
+                    case VOLUME:
+                        Console.WriteLine("geef het volume dat het wil op zetten");
+                        int volume = Convert.ToInt32(Console.ReadLine());
+                        ChangeSound(volume);
+                        Console.WriteLine($"je volume is nu {volume}");
+                        break;
+
+                        
+                    case CONTINUE:
+                        Continue();
+                        break;
+
+                }
+            }
+      
+
+            Console.ReadKey();
+        }
+        static public void ReadSong()
+        {
+            song = Console.ReadLine();
+        }
+        static public void PlaylistMenu()
+        {
+            Console.WriteLine("wat wil je doen?");
+            Console.WriteLine("type 'voegtoe' om een liedje to te voegen");
+            Console.WriteLine("type 'verwijder' om een liedje te verwijdere");
+            Console.WriteLine("type 'terug om terug te gaan");
+            string keuze = Console.ReadLine();        
+                switch (keuze)
+                {
+                    case VOEGTOE:
+                        Console.WriteLine("geef het liedje dat je wil toevoegen");
+                        song = Console.ReadLine();
+                        AddSongToList(song);
+                        break;
+
+                    case VERWIJDER:
+                        Console.WriteLine("geef het liedje dat je wil verwijderen");
+                        song = Console.ReadLine();
+                        RemoveSongFromList(song);
+                        break;
+
+                    case TERUG:
+                        geefStartmenu();
+                        break;
+                }           
+        }
+        static public void geefStartmenu()
+        {
+            Console.WriteLine(" .------------------------.");
+            Console.WriteLine(" |\\\\////////       Beau R |");
+            Console.WriteLine(" | \\/  __  ______  __     |");
+            Console.WriteLine(" |    /  \\|\\.....|/  \\    |");
+            Console.WriteLine(" |    \\__/|/_____|\\__/    |    ");
+            Console.WriteLine(" | play     pause    stop |   ");
+            Console.WriteLine(" |    ________________    |   ");
+            Console.WriteLine(" |___/_._o________o_._\\___|  ");
+
+            Console.WriteLine("");
+            Console.WriteLine("* type 'play' om een liedje te spelen  ");
+            Console.WriteLine("* type 'pause' om te pauzeren");
+            Console.WriteLine("* type 'stop' om te stoppen");
+            Console.WriteLine("* type 'volume' met je gewenste volume om het volume aan te passen");
+            Console.WriteLine("");
+            Console.WriteLine("* type 'bewerk' om playlist aanpassen" +
+                "");
+            Console.WriteLine("");
+            Console.WriteLine("Geef je Commando");
+            input = Console.ReadLine();
+        }
+        static public void AddSongToList(string song)
+        {
+            songList.Add(song);
+            File.WriteAllLines(destination, songList);
+        }
+        static public void RemoveSongFromList(string song)
+        {
+            songList.Remove(song);
+            File.WriteAllLines(destination, songList);
+        }
+        static public string[] ReadLinesFromText()
+        {
+            return linesArray = File.ReadAllLines(destination);
+        }
+        static public void ChangeSound(int volume)
+        {
+            player.settings.volume = volume;
+        }
+        static public void Mute(bool mute)
+        {
+            player.settings.mute = mute;
+        }
+        static public void Play(string song, string musicLocation)
+        {
+            player.URL = System.IO.Path.Combine(musicLocation, song);
+        }
+        static public void Continue()
+        {
+            player.controls.play();
+        }
+        static public void Pause()
+        {
+            player.controls.pause();
+        }
+        static public void Close()
+        {
+            Environment.Exit(0);
+        }
+        static public void LoadSongsFromFolder()
+        {
+            string[] music = Directory.GetFiles(@musicLocation);
+            foreach (string currentSong in music)
+            {
+                //string liedjesNaam;
+                song = currentSong.Substring(musicLocation.Length + 1);   // e.g. song.mp3
+
+                if (song.Contains(".mp3") || song.Contains(".flac"))
+                {
+                    //fill list with song path  e.g. c:\test\music\song.mp3
+                    songPathList.Add(currentSong);
+                  
+                    sizeOfList = songPathList.Count;
+                }
+            }
+
+        }
+        static public string GetNextSong()
+        {
+            int index = songPathList.FindIndex(a => a.Contains(song));
+            string nextSong = songPathList[index + 1];
+            song = nextSong;
+            return song;
+        }
+       static public string GetPrevious()
+        {
+            int index = songPathList.FindIndex(a => a.Contains(song));
+            string previousSong = songPathList[index - 1];
+            song = previousSong;
+            return song;
+        }
+    }
+}
---
+++ ProjectMusicPlayer-BeauRoels/WPFMusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WMPLib;
+
+namespace WPFMusicPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {           
+       //Windows media player
+        public static WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public static string docsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string destination = System.IO.Path.Combine(docsFolder, "playlist.txt ");
+        public string musicLocation = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+        //inputs
+        public static string input = "";
+        public static string song;
+        
+        //lists and arrays
+        public static string[] linesArray;
+        public static List<string> linesList = new List<string>();
+        public List<string> songPathList = new List<string>();
+        public int sizeOfList;
+
+        public MainWindow()
+        {
+            InitializeComponent();               
+            //er is een bug waarbij enkel het geselecteerde liedje van de combobox wordt toegevoegd aan de playslist
+        }
+        private void WindowMusicPlayer_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadSongsFromFolder();
+        }
+        private void btnShowExistingPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            ReadTextFile();
+        }    
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+            ReturnItemFromBox();
+            lblPlayingSong.Content = song;          
+            Play(song, musicLocation);
+        }
+        private void btnAddToPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            AddToListbox();
+            AddSongToList();
+        }
+       
+        private void btnContinue_Click(object sender, RoutedEventArgs e)
+        {
+            Continue();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            Pause();
+        }
+        private void btnMute_Click(object sender, RoutedEventArgs e)
+        {                       
+            Mute(true);              
+        }
+        private void btnUnmute_Click(object sender, RoutedEventArgs e)
+        {
+            Mute(false);
+        }
+        private void sldVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ChangeSound(Convert.ToInt32(sldVolume.Value));
+        }
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            player.URL = System.IO.Path.Combine(musicLocation, GetPrevious());
+            lblPlayingSong.Content = song.Substring(musicLocation.Length + 1); ;
+        }
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            player.URL = System.IO.Path.Combine(musicLocation, GetNextSong());
+            lblPlayingSong.Content = song.Substring(musicLocation.Length + 1); 
+        }
+        static public void ReadSong()
+        {
+            song = input;
+        }
+        static public void AddSongToList()
+        {
+            ReadLinesFromText();
+            linesList.Add(song);            
+            File.WriteAllLines(destination, linesList);
+        }     
+        static public string[] ReadLinesFromText()
+        {
+            return linesArray = File.ReadAllLines(destination);
+        }
+        static public void ChangeSound(int volume)
+        {
+            player.settings.volume = volume;
+        }      
+        static public void Mute(bool mute)
+        {
+            player.settings.mute = mute;
+        }
+        public void Play(string song, string musicLocation)
+        {
+            player.URL = System.IO.Path.Combine(musicLocation, song);
+        }
+        public void Continue()
+        {
+            player.controls.play();
+        }
+        public void Pause()
+        {
+            player.controls.pause();
+        }
+        public string GetNextSong()
+        {           
+            int index = songPathList.FindIndex(a => a.Contains(song));
+            string nextSong = songPathList[index+1];
+            song = nextSong;          
+            return song;
+        }
+        public string GetPrevious()
+        {
+            int index = songPathList.FindIndex(a => a.Contains(song));
+            string previousSong = songPathList[index-1];
+            song = previousSong;        
+            return song;
+        }
+        static public void Close()
+        {
+            Environment.Exit(0);
+        } 
+        public string ReturnItemFromBox()
+        {
+            return song = (string)cmbAllSongs.SelectedItem;
+        }         
+        public void AddToListbox()
+        {
+            ListBoxItem item = new ListBoxItem();
+            item.Content = ReturnItemFromBox();
+            listBoxPlaylist.Items.Add(item);        
+        }
+        public void ReadTextFile()
+        {
+            try
+            {
+                using (var sr = new StreamReader(destination))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        ListBoxItem item = new ListBoxItem();
+                        item.Content = sr.ReadLine();
+                        listBoxPlaylist.Items.Add(item);
+                    }
+                
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }                
+        public void LoadSongsFromFolder()
+        {
+            string[] music = Directory.GetFiles(@musicLocation);
+            foreach (string currentSong in music)
+            {
+                //string liedjesNaam;
+               song = currentSong.Substring(musicLocation.Length + 1);   // e.g. song.mp3
+
+                if (song.Contains(".mp3") || song.Contains(".flac") )
+                {
+                    //fill list with song path  e.g. c:\test\music\song.mp3
+                    songPathList.Add(currentSong);
+                    cmbAllSongs.Items.Add(song);
+                    sizeOfList = songPathList.Count;
+                }
+            }
+
+        }
+
+       
+    }
+}
